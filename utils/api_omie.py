a4a02d3d103b0ddfa508da9d63a2daa4714840b3
Aciem22 <gustavoaciem@gmail.com>
2026-03-30 11:24:22 -0300

    aumentando range de dias para pesquisar remessas

--- utils/api_omie.py
+++ utils/api_omie.py
@@ -9,7 +9,7 @@ APP_KEY = st.secrets["APP_KEY"]
 APP_SECRET = st.secrets["APP_SECRET"]
 
 
-ontem = datetime.now() - timedelta(days=3)
+ontem = datetime.now() - timedelta(days=7)
 ontem_formatado = ontem.strftime("%d/%m/%Y")
 
 # ==============================================================================
