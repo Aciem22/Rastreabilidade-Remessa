b79373e86f28a11177e679ac04838a5eacd65896
Aciem22 <gustavoaciem@gmail.com>
2026-03-30 11:40:43 -0300

    aumentando range de novo

--- utils/api_omie.py
+++ utils/api_omie.py
@@ -9,7 +9,7 @@ APP_KEY = st.secrets["APP_KEY"]
 APP_SECRET = st.secrets["APP_SECRET"]
 
 
-ontem = datetime.now() - timedelta(days=7)
+ontem = datetime.now() - timedelta(days=15)
 ontem_formatado = ontem.strftime("%d/%m/%Y")
 
 # ==============================================================================
