d532041068087e2787e5bec68b2f7b332cd28508
Aciem22 <gustavoaciem@gmail.com>
2026-04-27 14:15:04 -0300

    subindo upsert.py e conexão com banco

---
+++ api/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+from utils.neon_upsert import upsert_lotes
+
+app=FastAPI()
+
+# --------------------
+# SCHEMA 
+# --------------------
+
+class LoteItem(BaseModel):
+    sku:str
+    descricao: str
+    lote:str
+    validade: str
+
+# ------------------------
+# ENDPOINT
+# ------------------------
+
+@app.post("/upsert-lotes")
+def upsert_lotes_endpoint(dados:List[LoteItem]):
+    try:
+        # converte para dict (seu formato atual)
+        payload = [item.dict() for item in dados]
+
+        upsert_lotes(payload)
+
+        return {"status":"ok", "registros":len(payload)}
+    
+    except Exception as e:
+        return {"status": "erro", "mensagem": str(e)}
